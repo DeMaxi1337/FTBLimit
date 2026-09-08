8b551861a26849de50d2bf89dd02d5d3e9ba894a
Maxim <demaxi1100@gmail.com>
2026-09-09 00:23:47 +0300

    Remove auxiliary script files and add IntelliJ IDEA project configuration

--- get_meta.ps1
+++
@@ -1,2 +0,0 @@
-$meta = Invoke-RestMethod -Uri 'https://repo.papermc.io/repository/maven-public/io/papermc/paper/paper-api/1.21.1-R0.1-SNAPSHOT/maven-metadata.xml'
-$meta.metadata.versioning.snapshotVersions.snapshotVersion | Out-String | Write-Host
